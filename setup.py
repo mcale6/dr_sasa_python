9e052a29686835439ed321e1cb60741e2dbc57ca
mcale6 <mcichimaru6@gmail.com>
2024-10-26 01:11:52 +0200

    setup linux only

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from setuptools import setup
+from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext
 from pathlib import Path
 import platform
 import subprocess
 import sys
+import os
 
 try:
     from pybind11.setup_helpers import Pybind11Extension, build_ext
@@ -41,6 +42,23 @@ class CMakeBuild(build_ext):
                 '-DCMAKE_C_COMPILER=gcc',
                 '-DCMAKE_CXX_COMPILER=g++',
             ])
+        elif platform.system() == "Windows":
+            cmake_args.extend([
+                '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
+                    config.upper(), build_temp)
+            ])
+
+        # Add custom CMake options from environment
+        cmake_args.extend([
+            f'-D{k}={v}' for k, v in os.environ.items()
+            if k.startswith('CMAKE_')
+        ])
+
+        build_args = ['--config', config]
+        if platform.system() == "Windows":
+            build_args.extend(['--', '/m'])
+        else:
+            build_args.extend(['--', '-j4'])
 
         try:
             print("Configuring CMake...")
@@ -54,7 +72,7 @@ class CMakeBuild(build_ext):
 
             print("Building extension...")
             subprocess.run(
-                ['cmake', '--build', '.', '--config', config],
+                ['cmake', '--build', '.'] + build_args,
                 cwd=build_temp,
                 check=True,
                 capture_output=True,
@@ -62,7 +80,12 @@ class CMakeBuild(build_ext):
             )
 
             # Find and copy the built extension
-            built_ext = list(build_temp.rglob(f'*dr_sasa_py*.so'))[0]
+            if platform.system() == "Windows":
+                built_ext = list(build_temp.rglob(f'*dr_sasa_py*.pyd'))[0]
+            else:
+                built_ext = list(build_temp.rglob(f'*dr_sasa_py*.so'))[0]
+            
+            print(f"Copying {built_ext} -> {ext_fullpath}")
             self.copy_file(str(built_ext), str(ext_fullpath))
 
         except subprocess.CalledProcessError as e:
@@ -74,13 +97,13 @@ class CMakeBuild(build_ext):
             raise
 
 setup(
-    name='dr_sasa',
-    version='0.1',
-    author='mcale6',
-    description='Python bindings for dr_sasa',
-    long_description='',
-    ext_modules=[CMakeExtension('dr_sasa_py')],
-    cmdclass={'build_ext': CMakeBuild},
+    name="dr_sasa_py",
+    version="0.1.0",
+    author="mcaele6",
+    description="Python bindings for dr_sasa_n library",
+    packages=find_packages(),
+    ext_modules=[CMakeExtension("dr_sasa_py")],
+    cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     python_requires='>=3.7',
     install_requires=[
@@ -92,6 +115,12 @@ setup(
             'pytest',
             'pytest-cov',
             'flake8',
+            'black',
+            'mypy',
+        ],
+        'docs': [
+            'sphinx',
+            'sphinx_rtd_theme',
         ],
     },
     classifiers=[
