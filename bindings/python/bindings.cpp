492c5129119aa27e7d4874987798dae38c8fb96a
mcale6 <mcichimaru6@gmail.com>
2024-11-09 23:48:59 +0000

    more test

--- bindings/python/bindings.cpp
+++ bindings/python/bindings.cpp
@@ -16,7 +16,7 @@ PYBIND11_MODULE(dr_sasa_py, m) {
     
     py::register_exception<SASAError>(m, "SASAError");  // Register custom exceptions
     bind_atom_struct(m); // Bind core data structures
-    
+
     py::class_<SimpleSASA>(m, "SimpleSASA")
         .def(py::init<float, int>(),
              py::arg("probe_radius") = DEFAULT_PROBE_RADIUS,
@@ -24,7 +24,7 @@ PYBIND11_MODULE(dr_sasa_py, m) {
         .def("calculate", &SimpleSASA::calculate,
              py::arg("pdb_file"),
              py::arg("print_output") = false,
-             py::arg("output_name") = "output",
+             py::arg("output_name") = "simple",
              R"pbdoc(
                 Calculate SASA from PDB file.
 
@@ -39,7 +39,7 @@ PYBIND11_MODULE(dr_sasa_py, m) {
         .def("calculate_from_atoms", &SimpleSASA::calculate_from_atoms,
              py::arg("atoms"),
              py::arg("print_output") = false,
-             py::arg("output_name") = "output",
+             py::arg("output_name") = "simple",
              "Calculate SASA from list of atom_struct objects");
 
     py::class_<GenericSASA>(m, "GenericSASA")
@@ -51,7 +51,7 @@ PYBIND11_MODULE(dr_sasa_py, m) {
              py::arg("chains") = std::vector<std::vector<std::string>>(),
              py::arg("include_matrix") = true,
              py::arg("print_output") = false,
-             py::arg("output_name") = "output",
+             py::arg("output_name") = "generic",
              R"pbdoc(
                 Calculate SASA with chain-based analysis.
 
@@ -70,7 +70,7 @@ PYBIND11_MODULE(dr_sasa_py, m) {
              py::arg("chains") = std::vector<std::vector<std::string>>(),
              py::arg("include_matrix") = true,
              py::arg("print_output") = false,
-             py::arg("output_name") = "output",
+             py::arg("output_name") = "generic",
              "Calculate SASA from list of atom_struct objects");
 
     py::class_<DecoupledSASA>(m, "DecoupledSASA")
@@ -82,7 +82,7 @@ PYBIND11_MODULE(dr_sasa_py, m) {
              py::arg("chains") = std::vector<std::vector<std::string>>(),
              py::arg("include_matrix") = true,
              py::arg("print_output") = false,
-             py::arg("output_name") = "output",
+             py::arg("output_name") = "decoupled",
              R"pbdoc(
                 Calculate decoupled SASA analysis.
 
@@ -101,7 +101,7 @@ PYBIND11_MODULE(dr_sasa_py, m) {
              py::arg("chains") = std::vector<std::vector<std::string>>(),
              py::arg("include_matrix") = true,
              py::arg("print_output") = false,
-             py::arg("output_name") = "output",
+             py::arg("output_name") = "decoupled",
              "Calculate SASA from list of atom_struct objects");
 
     // Module attributes
