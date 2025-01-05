1aac9de24ee9ae1212703261f3eadbe1dafa73a0
mcale6 <88541612+mcale6@users.noreply.github.com>
2025-01-05 14:18:30 +0100

    Update install.sh
    update

--- install.sh
+++ install.sh
@@ -32,10 +32,9 @@ pip install "pybind11[global]" numpy pandas pytest pytest-cov
 if [ ! -d "dr_sasa_python" ] && [ ! -f "setup.py" ]; then
     echo "Cloning dr_sasa_python repository..."
     git clone --recursive https://github.com/mcale6/dr_sasa_python.git
-    cd dr_sasa_python
     # 6. Install the package in development mode
     echo "Installing dr_sasa_python in development mode..."
-    pip install -e .
+    cd dr_sasa_python && pip install -e .
 fi
 
 # 7. Build the project
